e75e4c1a33d9c654b081d502b144c63b8fdfcde1
Jakub Anielak <kuba@eskrytka.net.pl>
2025-06-26 14:32:59 +0200

    Add env. variables and create Procfile for gunicorn

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 load_dotenv(verbose=True)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -44,7 +44,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -284,4 +284,4 @@ def send_email(name, email, phone, message):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
